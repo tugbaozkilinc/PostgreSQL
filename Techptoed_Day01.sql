1fdf797d1fb4f2e3b1f9dfc1e4710a1e4a29f992
tugbaozkilinc <tugbaozkilinc22@gmail.com>
2022-12-02 15:15:42 +0300

    01/12/22

---
+++ Techptoed_Day01.sql
@@ -0,0 +1,52 @@
+-- DATABASE OLUSTURMA
+create database ece;
+create database tugba;
+
+-- DDL TABLE OLUSTURMA
+create table ogrenciler2
+(
+ogrenci_no char(7), isim varchar(20), soyisim varchar(25), not_ort real, kayit_tarih date
+);
+
+-- VAROLAN TABLODAN YENI BIR TABLO OLUSTURMA
+create table ogrenci_notlari as select isim, soyisim, not_ort from ogrenciler2;
+
+-- DML VERI EKLEME
+insert into ogrenciler2 values ('1234567', 'Ece', 'Can', 85.0, now());
+insert into ogrenciler2 values ('1234567', 'Ece', 'Can', 85.0, '2022-12-11');
+insert into ogrenciler2 (isim, soyisim) values ('Tugba', 'Omer');
+
+-- DQL select
+select * from ogrenciler2;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
