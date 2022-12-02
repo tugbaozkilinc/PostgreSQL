fd3835def7fe970387e096a2cd504730d9df82da
tugbaozkilinc <tugbaozkilinc22@gmail.com>
2022-12-03 00:35:11 +0300

    2022-12-02

---
+++ Day01_example.sql
@@ -0,0 +1,19 @@
+create table tedarikciler
+(
+tedarikci_id char(8), tedarikci_ismi varchar(30), tedarikci_adres varchar(30), ulasim_tarihi date
+);
+
+create table tedarikci_ziyaret as select tedarikci_ismi, ulasim_tarihi from tedarikciler;
+
+insert into tedarikciler values ('1234567', 'Ali Can', 'Izmir', now());
+insert into tedarikciler (tedarikci_id, tedarikci_ismi) values ('2345678', 'Mete Han');
+
+select * from tedarikciler;
+select * from tedarikci_ziyaret;
+
+insert into tedarikci_ziyaret values ('Ayse Nur', '2022-12-23');
+insert into tedarikci_ziyaret values (('Omer Faruk'), '2022-02-23');
+insert into tedarikciler (tedarikci_id, tedarikci_adres) values ('3456789', '2022-06-25');
+
+select * from tedarikciler;
+select * from tedarikci_ziyaret;
---
+++ Day02.sql
@@ -0,0 +1,205 @@
+create table ogrenciler5
+(
+ogrenci_no char(7), isim varchar(20), soyisim varchar(25), not_ort real, kayit_tarih date
+);
+
+create table notlar as select isim, not_ort from ogrenciler5;
+
+select * from notlar;
+
+insert into notlar values ('Ece Nur', 85.0);
+insert into notlar values ('Mete Han', 95.0);
+insert into notlar values ('Ali Kemal', 85.0);
+insert into notlar values ('Secil Cicek', 75.0);
+insert into notlar values ('Hakan Tan', 80.0);
+
+select * from notlar;
+
+create table talebeler
+(
+isim varchar(10), notlar real 
+);
+
+insert into talebeler values ('Ayca', 85.0);
+insert into talebeler values ('Mete', 95.0);
+insert into talebeler values ('Ali', 85.0);
+insert into talebeler values ('Secil', 75.0);
+insert into talebeler values ('Hakan', 80.0);
+
+-- CONSTRAINT
+-- UNIQUE
+create table ogrenciler7
+(
+ogrenci_no char(7) unique, 
+isim varchar(20) not null, 
+soyisim varchar(25), 
+not_ort real, kayit_tarih date
+);
+
+select * from ogrenciler7;
+
+insert into ogrenciler7 values ('1234567', 'Osman', 'Evren', 75.0, now());
+insert into ogrenciler7 values ('1234568', 'Ali', 'Veli', 75.0, now());
+insert into ogrenciler7 (ogrenci_no, soyisim, not_ort) values ('1234569', 'Can', 55.5); -- not null constraint
+
+-- PRIMARY KEY ATAMASI 1. yol
+create table ogrenciler8
+(
+ogrenci_no char(7) PRIMARY KEY, 
+isim varchar(20), 
+soyisim varchar(25), 
+not_ort real, 
+kayit_tarih date
+);
+
+-- PRIMARY KEY ATAMASI 2. yol
+create table ogrenciler9
+(
+ogrenci_no char(7), 
+isim varchar(20), 
+soyisim varchar(25), 
+not_ort real,
+kayit_tarih date,
+CONSTRAINT ogr primary key(ogrenci_no)
+);
+
+-- FOREIGN KEY
+/*
+“tedarikciler3” isimli bir tablo olusturun. Tabloda “tedarikci_id”, “tedarikci_ismi”, “iletisim_isim” field’lari olsun ve “tedarikci_id” yi Primary Key yapin.
+“urunler” isminde baska bir tablo olusturun “tedarikci_id” ve “urun_id” field’lari olsun ve
+“tedarikci_id” yi Foreign Key yapin.
+*/
+create table tedarikciler3
+(
+tedarikci_id char(5) primary key, tedarikci_ismi varchar(20), iletisim_isim varchar(20)
+);
+
+create table urunler
+(
+tedarikci_id char(5), urun_id char(5),
+foreign key (tedarikci_id) references tedarikciler3 (tedarikci_id)
+);
+
+select * from tedarikciler3;
+select * from urunler;
+
+/*
+“calisanlar” isimli bir Tablo olusturun. Icinde “id”, “isim”, “maas”, “ise_baslama” field’lari olsun. “id” yi Primary Key yapin, “isim” i Unique, “maas” i Not Null yapın.
+“adresler” isminde baska bir tablo olusturun.Icinde “adres_id”, “sokak”, “cadde” ve “sehir” fieldlari olsun. “adres_id” field‘i ile Foreign Key oluşturun.
+*/
+create table calisanlar
+(
+id varchar(15) PRIMARY KEY, isim varchar(30) UNIQUE, maas int NOT NULL, ise_baslama date
+);
+
+create table adresler
+(
+adres_id varchar(30), sokak varchar(30), cadde varchar(30), sehir varchar(30),
+FOREIGN KEY (adres_id) REFERENCES calisanlar(id)
+);
+
+INSERT INTO calisanlar VALUES ('10002', 'Mehmet Yılmaz', 12000, '2018-04-14');
+INSERT INTO calisanlar VALUES ('10008', null, 5000, '2018-04-14');
+INSERT INTO calisanlar VALUES ('10010', 'Mehmet Yılmaz', 5000, '2018-04-14'); -- unique constraint
+INSERT INTO calisanlar VALUES ('10004', 'Veli Han', 5000, '2018-04-14');
+INSERT INTO calisanlar VALUES ('10005', 'Mustafa Ali', 5000, '2018-04-14');
+INSERT INTO calisanlar VALUES ('10006', 'Canan Yaş', NULL, '2019-04-12'); -- not null constraint
+INSERT INTO calisanlar VALUES ('10003', 'CAN', 5000, '2018-04-14');
+INSERT INTO calisanlar VALUES ('10007', 'CAN', 5000, '2018-04-14'); -- unique constraint
+INSERT INTO calisanlar VALUES ('10009', 'cem', '', '2018-04-14'); -- syntax hatasi
+INSERT INTO calisanlar VALUES ('', 'osman', 2000, '2018-04-14');
+INSERT INTO calisanlar VALUES ('', 'osman can', 2000, '2018-04-14'); -- primary key
+INSERT INTO calisanlar VALUES ( '10002', 'ayse Yılmaz', 12000, '2018-04-14'); -- primary key
+INSERT INTO calisanlar VALUES ( null, 'filiz ' ,12000, '2018-04-14'); -- primary key
+
+-- Parent tabloda olmayan id ile child a ekleme yapamayiz
+INSERT INTO adresler VALUES ('10012', 'Ağa Sok', '30.Cad.', 'Antep');
+
+-- FK'ye null değeri atanabilir.
+INSERT INTO adresler VALUES (NULL, 'Ağa Sok', '30.Cad.', 'Antep');
+INSERT INTO adresler VALUES (NULL, 'Ağa Sok', '30.Cad.', 'Maraş');
+
+select * from calisanlar;
+select * from adresler;
+
+-- CHECK CONSTRAINT
+create table calisanlar1
+(
+id varchar(15) PRIMARY KEY, isim varchar(30) UNIQUE, maas int check (maas>1000) not null, ise_baslama date
+);
+
+insert into calisanlar1 values ('10002', 'Mehmet Yilmaz', 9000, '2018-04-14');
+insert into calisanlar1 values ('10002', 'Mehmet Yilmaz', 900, '2018-04-14'); -- check constraint
+
+create table calisanlar2
+(
+id varchar(15) PRIMARY KEY, isim varchar(30) UNIQUE, maas int check (maas>1000) not null, ise_baslama date
+);
+
+-- DQL WHERE KULLANIMI
+select * from calisanlar;
+select isim from calisanlar;
+
+-- calisanlar tablosundan maasi 5000 den buyuk olan isimleri listeleyiniz
+select isim, maas from calisanlar WHERE maas>5000;
+
+-- calisanlar tablosundan ismi Veli Han olan tum verileri listeleyiniz
+select * from calisanlar WHERE isim='Veli Han';
+
+-- calisanlar tablosundan maasi 5000 olan tum verileri listeleyiniz
+select * from calisanlar WHERE maas=5000;
+
+-- DML DELETE
+delete from calisanlar; -- eger parent table baska bir child table ile iliskili ise once child table silinmelidir
+delete from adresler;
+INSERT INTO adresler VALUES (NULL, 'Ağa Sok', '30.Cad.', 'Antep');
+INSERT INTO adresler VALUES (NULL, 'Ağa Sok', '30.Cad.', 'Maraş');
+select * from adresler;
+
+-- adresler tablosundan sehri Antep olan verileri silelim
+delete from adresler where sehir='Antep';
+
+-- ismi Nesibe Yilmaz veya Mustafa Bak olan kayıtları silelim.
+CREATE TABLE ogrenciler3
+(
+id int, isim VARCHAR(50), veli_isim VARCHAR(50), yazili_notu int
+);
+INSERT INTO ogrenciler3 VALUES(123, 'Ali Can', 'Hasan',75);
+INSERT INTO ogrenciler3 VALUES(124, 'Merve Gul', 'Ayse',85);
+INSERT INTO ogrenciler3 VALUES(125, 'Kemal Yasa', 'Hasan',85);
+INSERT INTO ogrenciler3 VALUES(126, 'Nesibe Yilmaz', 'Ayse',95);
+INSERT INTO ogrenciler3 VALUES(127, 'Mustafa Bak', 'Can',99);
+INSERT INTO ogrenciler3 VALUES(127, 'Mustafa Bak', 'Ali', 99);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
